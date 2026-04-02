af00e36311a388f37949470fcd413da6ed035322
Olesya <lesa76632@gmail.com>
2026-04-02 15:25:18 +0300

    Step 4: Create WeatherRepository with suspend functions

---
+++ app/src/main/java/com/example/myapplication/data/WeatherRepository.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.data
+
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+
+class WeatherRepository {
+    suspend fun fetchTemperature(): Int {
+        delay(2000)
+        return Random.nextInt(15, 35)
+    }
+
+    suspend fun fetchHumidity(): Int {
+        delay(1500)
+        return Random.nextInt(40, 80)
+    }
+    suspend fun fetchWindspeed(): Int{
+        delay(1000)
+        return Random.nextInt(0,20)
+    }
+}
